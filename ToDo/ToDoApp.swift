1ebc7fcb9ee15c50b45993478975211e1be9d71d
henriquepablo <pablo269704@gmail.com>
2026-05-23 16:04:30 -0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by pablo henrique on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoTests/ToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by pablo henrique on 23/05/26.
+//
+
+import Testing
+@testable import ToDo
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
